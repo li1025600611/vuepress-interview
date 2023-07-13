a18d0cc2308a23e3b9cc499b72f20364a71a57f3
vincy.li <vincy.li@cloudwise.com>
2023-07-13 11:23:48 +0800

    修改链接地址

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,96 +1,95 @@
 /*
  * Author  rhys.zhao
  * Date  2023-03-02 14:02:43
- * LastEditors  rhys.zhao
- * LastEditTime  2023-03-16 18:01:53
+ * LastEditors  Vincy.Li
+ * LastEditTime  2023-07-13 11:23:07
  * Description
  */
 
 module.exports = {
-  base: '/webpack-learn/',
-  lang: 'zh-CN',
-  head: [['link', { rel: 'icon', href: '/imgs/favicon.ico' }]],
-  title: '速学Webpack5',
-  description: '用最简单的方式学习Webpack5',
+  base: "/vuepress-interview/",
+  lang: "zh-CN",
+  head: [["link", { rel: "icon", href: "/imgs/favicon.ico" }]],
+  title: "速学Webpack5",
+  description: "用最简单的方式学习Webpack5",
 
-  port: '8888',
+  port: "8888",
 
   // 主题和它的配置
-  theme: '@vuepress/theme-default',
+  theme: "@vuepress/theme-default",
   themeConfig: {
-    logo: '/imgs/webpack-logo.svg',
-    repo: 'RhysZhao/webpack-learn',
-    repoLabel: '点亮⭐收藏',
+    logo: "/imgs/webpack-logo.svg",
+    repoLabel: "点亮⭐收藏",
     editLinks: true,
-    docsDir: 'docs',
-    editLinkText: '为该章节纠错',
-    lastUpdated: '上次更新',
+    docsDir: "docs",
+    editLinkText: "为该章节纠错",
+    lastUpdated: "上次更新",
     search: true,
     navbar: [
       {
-        text: '😶‍🌫️ 基础篇',
-        link: '/base/'
+        text: "😶‍🌫️ 基础篇",
+        link: "/base/",
       },
       {
-        text: '😶‍🌫️ 进阶篇',
-        link: '/senior/'
+        text: "😶‍🌫️ 进阶篇",
+        link: "/senior/",
       },
       {
-        text: '🤔 原理篇',
-        link: '/origin/'
-      }
+        text: "🤔 原理篇",
+        link: "/origin/",
+      },
     ],
     sidebar: {
-      '/base/': [
+      "/base/": [
         {
-          text: '基础篇',
+          text: "基础篇",
           children: [
-            '/base/README.md',
-            '/base/example.md',
-            '/base/plugin.md',
-            '/base/loader.md',
-            '/base/asset.md',
-            '/base/server.md',
-            '/base/env.md',
-            '/base/summary.md'
-          ]
-        }
+            "/base/README.md",
+            "/base/example.md",
+            "/base/plugin.md",
+            "/base/loader.md",
+            "/base/asset.md",
+            "/base/server.md",
+            "/base/env.md",
+            "/base/summary.md",
+          ],
+        },
       ],
-      '/senior/': [
+      "/senior/": [
         {
-          text: '进阶篇',
+          text: "进阶篇",
           children: [
-            '/senior/README.md',
-            '/senior/source-map.md',
-            '/senior/hmr.md',
-            '/senior/tree-shaking.md',
-            '/senior/code-split.md',
-            '/senior/react-cli.md',
-            '/senior/optimize.md'
-          ]
-        }
+            "/senior/README.md",
+            "/senior/source-map.md",
+            "/senior/hmr.md",
+            "/senior/tree-shaking.md",
+            "/senior/code-split.md",
+            "/senior/react-cli.md",
+            "/senior/optimize.md",
+          ],
+        },
       ],
-      '/origin/': [
+      "/origin/": [
         {
-          text: '原理篇',
-          children: ['/origin/README.md']
-        }
-      ]
-    }
+          text: "原理篇",
+          children: ["/origin/README.md"],
+        },
+      ],
+    },
   },
   plugins: [
     [
-      '@vuepress/plugin-search',
+      "@vuepress/plugin-search",
       {
         locales: {
-          '/': {
-            placeholder: 'Search'
+          "/": {
+            placeholder: "Search",
           },
-          '/zh/': {
-            placeholder: '搜索'
-          }
-        }
-      }
-    ]
-  ]
+          "/zh/": {
+            placeholder: "搜索",
+          },
+        },
+      },
+    ],
+  ],
 };
